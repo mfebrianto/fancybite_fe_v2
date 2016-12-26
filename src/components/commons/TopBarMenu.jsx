6c470ebdc78ab60db28cb90b6431e8a5bb42ab4d
michael febrianto <michaelfebrianto@gmail.com>
2016-12-26 22:46:01 +1100

    20161223 : add wave on menu

--- src/components/commons/TopBarMenu.jsx
+++ src/components/commons/TopBarMenu.jsx
@@ -5,22 +5,26 @@ class TopBarMenu extends React.Component {
 
   render(){
     return (
-      <Navbar collapseOnSelect className='fb-navbar'>
-        <div className='fb-navbar-logo'/>
-        <Navbar.Header>
-          <Navbar.Toggle className='fb-navbar-toggle'/>
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav className='fb-navbar-left'>
-            <NavItem eventKey={1} href="#">Menu</NavItem>
-            <NavItem eventKey={2} href="#" className='fb-nav-item-innder-left'>Download</NavItem>
-          </Nav>
-          <Nav pullRight className='fb-navbar-right'>
-            <NavItem eventKey={1} href="#" className='fb-nav-item-innder-right'>Special</NavItem>
-            <NavItem eventKey={2} href="#">Contact</NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div>
+        <Navbar collapseOnSelect className='fb-navbar'>
+          <div className='fb-navbar-logo'/>
+          <Navbar.Header>
+            <Navbar.Toggle className='fb-navbar-toggle'/>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav className='fb-navbar-left'>
+              <NavItem eventKey={1} href="#">Menu</NavItem>
+              <NavItem eventKey={2} href="#" className='fb-nav-item-innder-left'>Download</NavItem>
+            </Nav>
+            <Nav pullRight className='fb-navbar-right'>
+              <NavItem eventKey={1} href="#" className='fb-nav-item-innder-right'>Special</NavItem>
+              <NavItem eventKey={2} href="#">Contact</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className='wave-border'>
+        </div>
+      </div>
     )
   }
 }
